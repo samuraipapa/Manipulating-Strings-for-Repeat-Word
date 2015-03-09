64300b0855755f0b9c17a3be7b5d1bf697c963d3
samuraipapa <yury.gitman@gmail.com>
2015-03-09 16:52:56 -0400

    Acts Crazy

---
+++ NSScannerExample/StarterString.swift
@@ -0,0 +1,12 @@
+//
+//  StarterString.swift
+//  NSScannerExample
+//
+//  Created by GrownYoda on 3/9/15.
+//  Copyright (c) 2015 yuryg. All rights reserved.
+//
+
+import Foundation
+
+
+var starterString = "CARACAS, Venezuela — He thunders about conspiracies and assassination plots. He says that he sleeps with both eyes open. Few Venezuelans even know where he lives.But no matter the dangers, President Nicolás Maduro says that no one will scare him, fool him or divert him from carrying out the mission that the “eternal Commander Chávez” has given him “until the end of the end of the roads, now and forever.”Mr. Maduro came into office seeking to imitate his charismatic predecessor and mentor, Hugo Chávez, in nearly every way: the way he talked, the way he dressed and in his fulminations against American imperialism.But now, two years after the death of Mr. Chávez, with his country sinking deeper into an economic crisis, what was once Mr. Maduro’s greatest advantage — his absolute loyalty to the late leader — may have become his greatest handicap.Continue reading the main storyRELATED COVERAGEObama Freezes Assets of Seven Venezuelan OfficialsMARCH 9, 2015A sample bank note with the face of President Nicolás Maduro of Venezuela and the word “devaluated.”Amid a Slump, a Crackdown for VenezuelaFEB. 22, 2015Oil Cash Waning, Venezuelan Shelves Lie Bare JAN. 29, 2015Vice President Nicolás Maduro on Wednesday consoled a supporter of President Hugo Chávez.A Leader’s Cry in Venezuela: ‘I Am Chávez’MARCH 6, 2013Hugo Chávez | 1954-2013: Hugo Chávez, Venezuela’s Polarizing Leader, Dies at 58MARCH 5, 2013“The government inaction, the inertia, comes from a belief that you find in Nicolás Maduro and his government about defending Chávez’s legacy, as if nothing that Chávez left can be touched, nothing can be changed or corrected because that would be considered a betrayal,” said Victor Álvarez, a leftist economist and former government minister under Mr. Chávez."
--- NSScannerExample/ViewController.swift
+++ NSScannerExample/ViewController.swift
@@ -2,17 +2,34 @@
 //  ViewController.swift
 //  NSScannerExample
 //
-//  Created by GrownYoda on 3/9/15.
+//  Created yuryg on 3/9/15.
 //  Copyright (c) 2015 yuryg. All rights reserved.
 //
 
+//
+
 import UIKit
+
+
+// from
+// http://stackoverflow.com/questions/25678373/swift-split-a-string-into-an-array
+
+extension String {
+    var wordList:[String] {
+        return "".join(componentsSeparatedByCharactersInSet(NSCharacterSet.punctuationCharacterSet())).componentsSeparatedByString(" ")
+    }
+}
+
+
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scannerEx1()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +38,41 @@ class ViewController: UIViewController {
     }
 
 
+    
+    func scannerEx1(){
+        
+        var i = 0
+        
+        var  myWordList = starterString.wordList  // ["First", "Last"]
+        var repeatedWordArray = ["hello"]
+        
+        
+        var myDictionary: [String:Array] = [ "hello" : [1,1,1] ]
+        
+        
+        for word in myWordList {
+
+            
+            for checkWord in myWordList {
+                
+                   if (checkWord == word){
+                   repeatedWordArray.append(word)
+                    
+                      var repeatedWord = checkWord
+                            repeatedWordArray.append(repeatedWord)
+
+                        
+                    }
+                    
+                    println(repeatedWordArray)
+
+                }
+            }
+            
+        }
+
+        
+        
+        
 }
 
