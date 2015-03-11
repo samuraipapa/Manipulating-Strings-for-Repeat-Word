b902c99e4d94cd914c5a3607b39a88ef133a8685
samuraipapa <yury.gitman@gmail.com>
2015-03-11 12:18:57 -0400

    added attributed text code

--- NSScannerExample/ViewController.swift
+++ NSScannerExample/ViewController.swift
@@ -38,7 +38,28 @@ class ViewController: UIViewController {
     }
 
  
-    
+    func printAttributedText(){
+        
+        // Code orginally from http://www.ioscreator.com/tutorials/attributed-strings-tutorial-ios8-swift
+        // 1
+        
+        let string = "Testing Attributed Strings" as NSString
+        var attributedString = NSMutableAttributedString(string: string)
+        
+        // 2
+        let firstAttributes = [NSForegroundColorAttributeName: UIColor.blueColor(), NSBackgroundColorAttributeName: UIColor.yellowColor(), NSUnderlineStyleAttributeName: 1]
+        let secondAttributes = [NSForegroundColorAttributeName: UIColor.redColor(), NSBackgroundColorAttributeName: UIColor.blueColor(), NSStrikethroughStyleAttributeName: 1]
+        let thirdAttributes = [NSForegroundColorAttributeName: UIColor.greenColor(), NSBackgroundColorAttributeName: UIColor.blackColor(), NSFontAttributeName: UIFont.systemFontOfSize(40)]
+        
+        // 3
+        attributedString.addAttributes(firstAttributes, range: string.rangeOfString("Testing"))
+        attributedString.addAttributes(secondAttributes, range: string.rangeOfString("Attributed"))
+        attributedString.addAttributes(thirdAttributes, range: string.rangeOfString("Strings"))
+        
+        // 4
+        attributedLabel.attributedText = attributedString
+    }
+    }
     
     func scannerEx1(){
         
